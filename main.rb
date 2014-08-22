a2f7cb92d76c531afdb06c22173bd1dfac8e0edc
wemee <wemee7012@gmail.com>
2014-08-22 12:08:32 +0800

    物件導向一些些

--- main.rb
+++ main.rb
@@ -1,10 +1,18 @@
+class String
+	def encrypt key_number
+		encryptedCodes = self.split("").map{|c| c.ord + key_number}
+		encryptedString = encryptedCodes.pack("U" * encryptedCodes.length)
+	end
+
+	def decrypt key_number
+		plainCodes = self.split("").map{|c| c.ord - key_number}
+		plainString = plainCodes.pack("U" * plainCodes.length)
+	end
+end
+
 plainString = "大家好 我叫蔡至勇"
-magicNumber = 1234
+encryptedString = plainString.encrypt 1234
+puts encryptedString
 
-encryptedCodes = plainString.split("").map{|c| c.ord + magicNumber}
-
-plainCodes = encryptedCodes.map{|c| c-magicNumber}
-
-plainString = plainCodes.pack("U" * plainCodes.length)
-
-p plainString
+decryptedString = encryptedString.decrypt 1234
+puts(decryptedString)
